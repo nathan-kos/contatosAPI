42a664d1dad15c85f673556d8c3ef69075d52f1b
nathan ks <nathanks0210@gmail.com>
2024-10-05 16:24:18 -0300

    fix: update contact validators

--- src/modules/Contact/service/UpdateContact.py
+++ src/modules/Contact/service/UpdateContact.py
@@ -10,10 +10,10 @@ class UpdateContact:
         if not contact:
             raise ValueError("Contact not found");
     
-        if email & "@" not in email:
+        if email and "@" not in email:
             raise ValueError("Invalid email address");
     
-        if phone & len(phone) != 10:
+        if phone and len(phone) < 9:
             raise ValueError("Invalid phone number");
 
         return await self.contact_repository.update_contact(contact_id, name, email, phone);
