44c6ce7bd0a90d0226d96a0bebee65da56ad9ce9
nathan ks <nathanks0210@gmail.com>
2024-10-05 13:33:14 -0300

    feat: Add Contact entity class

---
+++ src/modules/Contact/entity/Contact.py
@@ -0,0 +1,8 @@
+class Contact:
+    def __init__(self, con_id, con_name, con_email, con_phone, con_created, con_updated):
+        self.con_id = con_id
+        self.con_name = con_name
+        self.con_email = con_email
+        self.con_phone = con_phone
+        self.con_created = con_created
+        self.con_updated = con_updated
