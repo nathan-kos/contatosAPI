c6b098a4b4478114bbf6f36150b244199e970ac0
nathan ks <nathanks0210@gmail.com>
2024-10-06 14:26:56 -0300

    feat: add exemple to swagger docs

--- src/modules/Contact/controller/Validator/Models.py
+++ src/modules/Contact/controller/Validator/Models.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class ContactModel(BaseModel):
-    con_name: str =  Field(...);
-    con_email: str = Field(..., pattern="[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+");
-    con_phone: str = Field(min_length=10, max_length=16);
+    con_name: str =  Field(..., example="Nathan Santos");
+    con_email: str = Field(..., pattern="[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+", example="nathan@gmail.com");
+    con_phone: str = Field(min_length=10, max_length=16, example="99999999999");
 
 
 class ContactCreateModel(ContactModel):
