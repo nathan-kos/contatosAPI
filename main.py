0db9386c66137bb1ba35914fbef3be8c4f708b09
nathan ks <nathanks0210@gmail.com>
2024-10-06 17:34:47 -0300

    feat: add cors to permit same origin request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.shared.database.index  import init, shutdown
 from src.modules.Contact.controller import ContactController
 
@@ -10,6 +11,14 @@ async def lifespan(app: FastAPI):
     await shutdown()
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include the routes
 app.include_router(ContactController.router, prefix="/contacts")
